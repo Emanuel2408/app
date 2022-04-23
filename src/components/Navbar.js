d20d9ce30910e3bbaa7ec276bf644bfda8adc086
Emanuel2408 <emanuelmisic64@gmail.com>
2022-04-23 15:24:29 +0200

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,10 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <nav>
-        <p>my travel journal.</p>
+        <p>
+          <span class="material-symbols-outlined">public</span>my travel
+          journal.
+        </p>
       </nav>
     </div>
   );
